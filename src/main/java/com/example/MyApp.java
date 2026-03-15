dd621d0c17aa4f52b97e54cd4282474b7004df68
Jason Kramer <pgmn6kxn7k@privaterelay.appleid.com>
2026-03-15 18:25:02 +0000

    modified:   target/maven-status/maven-compiler-plugin/compile/default-compile/removedFiles.lst

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -13,8 +13,32 @@ public class MyApp implements StreamingApplication {
         // For example: dag.addStream("inputStream", source.output, operator.input);
     }
 
-    public static void main(String[] args) throws Exception {
-        StramAppLauncher appLauncher = new StramAppLauncher();
-        appLauncher.run(new MyApp(), args);
+package com.example;
+
+import org.apache.hadoop.conf.Configuration;
+import com.datatorrent.api.DAG;
+import com.datatorrent.api.StreamingApplication;
+import com.datatorrent.stram.StramAppLauncher;
+
+public class MyApp implements StreamingApplication {
+    @Override
+    public void populateDAG(DAG dag, Configuration conf) {
+        MyOperator operator = dag.addOperator("myOperator", MyOperator.class);
+        // In a real app, connect inputs/outputs to sources/sinks
+        // For example: dag.addStream("inputStream", source.output, operator.input);
     }
+
+package com.example;
+
+public class MyApp {
+    public static void main(String[] args) throws Exception {
+        // For debugging purposes, test the operator directly
+        MyOperator operator = new MyOperator();
+        // Simulate processing some input
+        operator.process("hello");
+        operator.process("world");
+        System.out.println("Debug: Operator processed inputs");
+    }
+}
+}
 }
--- src/main/java/com/example/MyOperator.java
+++ src/main/java/com/example/MyOperator.java
@@ -1,17 +1,9 @@
 package com.example;
 
-import com.datatorrent.api.DefaultInputPort;
-import com.datatorrent.api.DefaultOutputPort;
-import com.datatorrent.common.util.BaseOperator;
-
-public class MyOperator extends BaseOperator {
-    public final transient DefaultInputPort<String> input = new DefaultInputPort<String>() {
-        @Override
-        public void process(String tuple) {
-            // Process incoming data - convert to uppercase
-            output.emit(tuple.toUpperCase());
-        }
-    };
-
-    public final transient DefaultOutputPort<String> output = new DefaultOutputPort<>();
+public class MyOperator {
+    public void process(String tuple) {
+        // Process incoming data - convert to uppercase
+        String result = tuple.toUpperCase();
+        System.out.println("Processed: " + result);
+    }
 }
