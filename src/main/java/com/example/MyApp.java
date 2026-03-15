50f962d06b261ec139390f4e983c445775c43c40
Jason Kramer <pgmn6kxn7k@privaterelay.appleid.com>
2026-03-15 18:35:12 +0000

    modified:   target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst added new file:   target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -1,35 +1,5 @@
 package com.example;
 
-import org.apache.hadoop.conf.Configuration;
-import com.datatorrent.api.DAG;
-import com.datatorrent.api.StreamingApplication;
-import com.datatorrent.stram.StramAppLauncher;
-
-public class MyApp implements StreamingApplication {
-    @Override
-    public void populateDAG(DAG dag, Configuration conf) {
-        MyOperator operator = dag.addOperator("myOperator", MyOperator.class);
-        // In a real app, connect inputs/outputs to sources/sinks
-        // For example: dag.addStream("inputStream", source.output, operator.input);
-    }
-
-package com.example;
-
-import org.apache.hadoop.conf.Configuration;
-import com.datatorrent.api.DAG;
-import com.datatorrent.api.StreamingApplication;
-import com.datatorrent.stram.StramAppLauncher;
-
-public class MyApp implements StreamingApplication {
-    @Override
-    public void populateDAG(DAG dag, Configuration conf) {
-        MyOperator operator = dag.addOperator("myOperator", MyOperator.class);
-        // In a real app, connect inputs/outputs to sources/sinks
-        // For example: dag.addStream("inputStream", source.output, operator.input);
-    }
-
-package com.example;
-
 public class MyApp {
     public static void main(String[] args) throws Exception {
         // For debugging purposes, test the operator directly
@@ -39,6 +9,4 @@ public class MyApp {
         operator.process("world");
         System.out.println("Debug: Operator processed inputs");
     }
-}
-}
 }
